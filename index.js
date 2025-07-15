1387a748be87e12db6e60b191f5edd4343a71d05
Oppong Coffie <oppongcoffie27@gmail.com>
2025-07-15 16:48:21 +0000

    assignment to spreedsheet2

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.use("/students", studentRoutes);
 app.use("/auth", authRoutes);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_LOCAL_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
 }).then(() => {
   console.log("Connected to MongoDB");
 }).catch((err) => {
